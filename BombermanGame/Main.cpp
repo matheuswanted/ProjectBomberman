cdefb1c6ec8a0886c74c6983d7dea1cec7dfdf62
EDUARDO CURCINO <eduardo-curcino@hotmail.com>
2017-06-12 19:03:11 -0300

    change window text

--- BombermanGame/Main.cpp
+++ BombermanGame/Main.cpp
@@ -7,7 +7,7 @@ using namespace cgf;
 int main(int argc, char **argv)
 {
     cgf::Game game(5,30);
-	game.init("Framework com estados, sprites e tiling",544,512,false);
+	game.init("Super Bomberman",544,512,false);
     GamePlayState state = GamePlayState();
     game.changeState(&state);
 
